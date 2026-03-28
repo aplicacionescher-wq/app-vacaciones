2e29bc02f5f7355dcd9b4eb0e39687889fee6548
aplicacionescher-wq <aplicacionescher@gmail.com>
2026-03-27 18:38:28 -0600

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// TU CONFIG
+const firebaseConfig = {
+  apiKey: "AIzaSyCrFSplvyoXZy_mgkVzG7e1VuPCPXM3gcs",
+  authDomain: "vacaciones-app-7353a.firebaseapp.com",
+  projectId: "vacaciones-app-7353a",
+  storageBucket: "vacaciones-app-7353a.firebasestorage.app",
+  messagingSenderId: "829112426227",
+  appId: "1:829112426227:web:064a78429796e888d9a186"
+};
+
+// INICIALIZAR
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 window.login = () => {
   rolActual = document.getElementById("rol").value;
   document.getElementById("login").style.display = "none";
